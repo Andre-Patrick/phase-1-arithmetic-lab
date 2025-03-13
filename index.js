ce883a5d5fd08acc9ff41b56db2c95b6faed447f
Patrick <patrick.andre@student.moringaschool.com>
2025-03-13 23:23:25 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Variables for multiply
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+// Variable for random
+const random = Math.ceil(Math.random() * 1000);
+
+// Variables for mod
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Variable for max
+const max = Math.max(5, 10, 20, 15);
